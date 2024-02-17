eb2804aea52f4dd8416651ef09fd5c7c21988ef9
Simon Yuen <simonyuen1999@hotmail.com>
2024-02-16 23:28:23 -0500

    Use <select> to pick: container, vuewContainerRef, AppRefview to insert & clear the #container

--- src/app/modules/lazy/dash.component.ts
+++ src/app/modules/lazy/dash.component.ts
@@ -7,6 +7,7 @@ import {
     ViewChild,
     ViewContainerRef
 } from '@angular/core';
+// import { NgForOf } from '@angular/common';
 
 import { TchComponent } from './tch.component';
 import { HelloComponent } from './hello.component';
@@ -14,13 +15,20 @@ import { HelloComponent } from './hello.component';
 @Component({
   selector: 'dash-root',
   template: `
-    <button (click)="loadComponent()">Load Component</button>
+    <button (click)="loadComponent()">Load Component</button> &nbsp; &nbsp;
+    <select id='sSelect' name='sSelect' (change)="loadComponent()">
+      <option value="container">container</option>
+      <option value="viewContainerRef">viewContainerRef</option>
+      <option value="appRef">appRef</option>
+    </select>
     <ng-container #container></ng-container>
-  `
+  `,
 })
+
 export class DashComponent {
     
   bAlternative = true;
+  sSelect:string = 'container';
 
   @ViewChild('container', { read: ViewContainerRef }) container!: ViewContainerRef;
 
@@ -28,10 +36,15 @@ export class DashComponent {
     private componentFactoryResolver: ComponentFactoryResolver,
     private injector: Injector,
     private appRef: ApplicationRef,
-    private el: ElementRef
+    private el: ElementRef,
+    private viewContainerRef: ViewContainerRef
   ) {}
 
   loadComponent() {
+    const sel = document.getElementById('sSelect') as HTMLSelectElement;
+    this.sSelect = sel.options[sel.selectedIndex].value;
+    console.log('--------------------------'+ this.sSelect +'--------------------------------');
+
     if (this.bAlternative) {
       this.loadComponent$(TchComponent);
     } else {
@@ -47,16 +60,54 @@ export class DashComponent {
 
     // create a component instance using the factory and the injector
     const componentRef = componentFactory.create(this.injector);
+  
+    const nContainer = this.container.length;
+    const nViewContainerRef = this.viewContainerRef.length;
+    const nAppRef = this.appRef.components.length;
+    const nAppRefview = this.appRef.viewCount
 
-    // Alternative way: attach the component to the view container
-    // this.container.insert(componentRef.hostView);
+    console.log('nContainer: ' + nContainer + ', nViewContainerRef: ' + nViewContainerRef + ', nAppRef: ' + nAppRef + ', nAppRefview: ' + nAppRefview);
 
-    this.appRef.attachView(componentRef.hostView);
-    this.el.nativeElement.appendChild(componentRef.location.nativeElement);
+    // ------------------------------------------------------------
+    if ( this.sSelect === 'container' ) {
+
+      console.log( 'container.insert(componentRef.hostView)');
+
+      if (this.container.length > 0) {
+        console.log('container.length > 0, the container.clear()');  
+        this.container.clear();
+      } 
+      this.container.insert(componentRef.hostView);
+
+    // ------------------------------------------------------------
+   
+    } else if ( this.sSelect === 'viewContainerRef' ) {
+
+      console.log( 'viewContainerRef.insert(componentRef.hostView)');
+
+      if (this.viewContainerRef.length > 0) {  
+        console.log('viewContainerRef.length > 0, the viewContainerRef.clear()');
+        this.viewContainerRef.clear();
+      }
+      this.viewContainerRef.insert(componentRef.hostView);
+
+    // ------------------------------------------------------------
+   
+    } else if ( this.sSelect === 'appRef' ) {
+
+      console.log( 'appRef.attachView(componentRef.hostView) and el.nativeElement.appendChild(componentRef.location.nativeElement)');
+
+      if (this.appRef.viewCount > 0) { 
+        console.log('appRef.viewCount > 0, the appRef.detachView()');
+        this.appRef.detachView(componentRef.hostView);
+      }
+
+      this.appRef.attachView(componentRef.hostView);
+      this.el.nativeElement.appendChild(componentRef.location.nativeElement);
+    }
 
     componentRef.onDestroy(() => {
       console.log('componentRef.onDestroy()');
-      this.appRef.detachView(componentRef.hostView);
     });
   }
 }
--- src/app/modules/lazy/lazy.module.ts
+++ src/app/modules/lazy/lazy.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LazyRoutingModule } from './lazy-routing.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
-    LazyRoutingModule
+    LazyRoutingModule,
+    FormsModule
   ]
 })
 export class LazyModule { }
